4ce298dba8014e9ba959dcd82b3b8986f3ae6b39
Agnieszka <agnie.jastrzebska@gmail.com>
2026-03-10 17:38:46 +0100

    Klasse Auto

---
+++ Auto.cs
@@ -0,0 +1,13 @@
+public class Auto
+{
+    public string Marke;
+    public int Baujahr;
+
+    public void Fahren(int geschwindigkeit)
+    {
+        Console.WriteLine($"Auto fährt {geschwindigkeit} km/h.");
+    }
+
+}
+
+    
