93def7ad17fe1cce3c6661af1706d6cd67d10323
Agnieszka <agnie.jastrzebska@gmail.com>
2026-03-10 17:36:12 +0100

    Aufgabe 5

--- Program.cs
+++ Program.cs
@@ -1,35 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 //Aufgaben 1
+//1.1
 
-/*
 string name = "Agnieszka";
 int alter= 38;
 string stadt ="Lippstadt";
 Console.WriteLine($"Mein Name ist {name}, ich bin {alter} Jahre alt und wohne in {stadt}.");
 
+//1.2
 Console.WriteLine("Hallo, wie heißt du?");
 string userName = Console.ReadLine();
 Console.WriteLine($"Hallo {userName}, schön dich kennenzulernen!");   
 
+//1.3
 Console.WriteLine("Wie alt bist du?");
 string userAge= Console.ReadLine();
 Console.WriteLine($"Du bist {userAge} alt.");
 
+//1.4
 int a = 10;
 int b= 20;
 int sume= a+b;
 int differnz=Math.Abs(a-b);
 Console.WriteLine($"Sume ist {sume} und differenz ist {differnz}");
 
-
+// 1.5
 Console.WriteLine("Gib eine Zahlen ein:");
 int userZahl1= Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Gib  noch eine Zahlen ein:");
 int userZahl2= Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Sume von deine Zahlen ist: {userZahl1 + userZahl2}");
 
-
+//1.6
 Console.WriteLine("Gib eine Zahl ein:");
 int userZahl3= Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(userZahl3 * userZahl3);
@@ -38,9 +41,9 @@ Console.WriteLine(userZahl3 * userZahl3);
 //Aufgaben 2
 
 Console.WriteLine("Gib mir bitte eine Zahl");
-int userZahl1=Convert.ToInt32(Console.ReadLine());
+int userZahl7=Convert.ToInt32(Console.ReadLine());
 
-if (userZahl1 >10)
+if (userZahl7 >10)
     Console.WriteLine("Gross");
 
 else
@@ -77,13 +80,15 @@ Console.WriteLine(userZahl % 2 ==0? "Gerade": "Ungerade");
 
 
 Console.WriteLine("Gib eine Zahl ein:");
-int userZahl= Convert.ToInt32(Console.ReadLine());
+int userZahl9= Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Gib noch eine Zahl ein");
-int userZahl2= Convert.ToInt32(Console.ReadLine());
-int maxZahl= Math.Max(userZahl, userZahl2);
+int userZahl10= Convert.ToInt32(Console.ReadLine());
+int maxZahl= Math.Max(userZahl9, userZahl10);
 Console.WriteLine(maxZahl);
 
+
 //Aufgaben 3
+//3.1
 
 int i;
 string liste1="";
@@ -95,7 +100,7 @@ for(i=1;i <=20; i++)
 }
 Console.WriteLine(liste1);
 
-
+//3.2
 int i2;
 string liste2="";
 
@@ -110,49 +115,49 @@ Console.WriteLine(liste2);
 
 //das gleiche wie oben aber anders
 string liste5="";
-for (int i=2; i<=50;i+=2)
+for (int h=2; h<=50;h+=2)
 {
-    liste5+=i + ",";
+    liste5+=h + ",";
 }
 Console.WriteLine(liste5);
 
+//3.3
 
+int suma=0;
 
-int sume=0;
-
-for (int i=1; i <= 100; i++)
+for (int j=1; j <= 100; j++)
 {
-    sume+=i;
+    suma+=j;
     
 }
-Console.WriteLine(sume);
+Console.WriteLine(suma);
 
+//3.4
 
 Console.WriteLine("Gib eine Zahl ein:");
-int userZahl= Convert.ToInt32(Console.ReadLine());  
-for (int i=1; i <=userZahl; i++)
+int userrZahl= Convert.ToInt32(Console.ReadLine());  
+for (int c=1; c <=userrZahl; c++)
 {
-    Console.WriteLine(i);
+    Console.WriteLine(c);
 }
 
-Console.Write("Gib eine Zahl ein: ");
 
+//3.5
 int zahl =7;
 
-int i = 1;
+int d = 1;
 
-while (i <= 10)
+while (d <= 10)
 {
-    Console.WriteLine($"{zahl} x {i} = {zahl * i}");
-    i++;
+    Console.WriteLine($"{zahl} x {d} = {zahl * d}");
+    d++;
+}
+//3.6
+for (int f=10; f >=1; f--)
+{
+    Console.WriteLine(f);
 }
 
-for (int i=10; i >=1; i--)
-{
-    Console.WriteLine(i);
-    
-}
-*/
 //Aufgabe 4
 //4.1
 
@@ -194,3 +199,38 @@ int NoteBerechen (int punkte)
     else if (punkte >= 50) return 4;
     else return 5;
 }
+//Aufgaben 5
+//5.2  
+
+
+Auto auto1 = new Auto();
+auto1.Marke = "Toyota Yaris";
+auto1.Baujahr = 2025;
+Console.WriteLine($"Marke ist: {auto1.Marke} und Baujahr ist: {auto1.Baujahr}");
+
+//5.3
+Auto auto2= new Auto() { Marke = "Audi", Baujahr = 1995 };
+Console.WriteLine($"Marke ist: {auto2.Marke} und Baujahr ist: {auto2.Baujahr}");
+Console.WriteLine(auto2);
+
+//5.5
+auto2.Fahren(158);
+auto1.Fahren(52);
+
+//5.6
+List<Auto> autoListe = new List<Auto>()
+{
+  new Auto() { Marke = "BMW v7", Baujahr = 2025 },
+  new Auto() { Marke = "Audi a4", Baujahr = 2024 },
+  new Auto() { Marke = "VW Golf 5", Baujahr = 2023 },   
+  new Auto() { Marke = "Ford Fiesta", Baujahr = 2022 },
+  new Auto() { Marke = "Fiat Punto", Baujahr = 2021 }    
+};
+
+Console.WriteLine("Autos in der Liste:");
+foreach (var auto in autoListe)
+{
+    Console.WriteLine($"Marke: {auto.Marke}, Baujahr: {auto.Baujahr}");
+}
+
+
