7c150290df08b30b720247e30ce1b50e99764602
Agnieszka <agnie.jastrzebska@gmail.com>
2026-03-10 13:35:25 +0100

    Aufagebe 2

--- Program.cs
+++ Program.cs
@@ -33,12 +33,56 @@ Console.WriteLine($"Sume von deine Zahlen ist: {userZahl1 + userZahl2}");
 Console.WriteLine("Gib eine Zahl ein:");
 int userZahl3= Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(userZahl3 * userZahl3);
-*/
+
 
 //Aufgaben 2
 
+Console.WriteLine("Gib mir bitte eine Zahl");
+int userZahl1=Convert.ToInt32(Console.ReadLine());
+
+if (userZahl1 >10)
+    Console.WriteLine("Gross");
+
+else
+    Console.WriteLine("Klein.");
+
+Console.WriteLine("Wie viel Punkte hast du ?");
+int punktezahl= Convert.ToInt32(Console.ReadLine());
+
+if (punktezahl <=50)
+    Console.WriteLine("bestanden");
+else
+    Console.WriteLine("nicht bestanden");
 
 
+string bewertung2=punktezahl switch
+{
+    >=90 => "Note 1",
+    >=80 => "Note 2",
+    >=70 => "Note 3",
+    >=60 => "Note 4",
+    _ => "Nicht bestanden, tut mir leid"
+};
+Console.WriteLine(bewertung2);    
+
+
+Console.WriteLine("Wie alt bist du?");
+int userAlter=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(userAlter <18? "Minderjahrig": "Volljahrig");
+
+Console.WriteLine("Gib eine Zahl ein:");
+int userZahl= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(userZahl % 2 ==0? "Gerade": "Ungerade");
+
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Gib eine Zahl ein:");
+int userZahl= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Gib noch eine Zahl ein");
+int userZahl2= Convert.ToInt32(Console.ReadLine());
+int maxZahl= Math.Max(userZahl, userZahl2);
+Console.WriteLine(maxZahl);
+*/
 
 
 
