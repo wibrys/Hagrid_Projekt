06d7e887111a3ad66aab2044404c8620bdde1b88
Agnieszka <agnie.jastrzebska@gmail.com>
2026-03-10 13:08:37 +0100

    Aufgabe 1

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Aufgaben 1
+
+/*
+string name = "Agnieszka";
+int alter= 38;
+string stadt ="Lippstadt";
+Console.WriteLine($"Mein Name ist {name}, ich bin {alter} Jahre alt und wohne in {stadt}.");
+
+Console.WriteLine("Hallo, wie heißt du?");
+string userName = Console.ReadLine();
+Console.WriteLine($"Hallo {userName}, schön dich kennenzulernen!");   
+
+Console.WriteLine("Wie alt bist du?");
+string userAge= Console.ReadLine();
+Console.WriteLine($"Du bist {userAge} alt.");
+
+int a = 10;
+int b= 20;
+int sume= a+b;
+int differnz=Math.Abs(a-b);
+Console.WriteLine($"Sume ist {sume} und differenz ist {differnz}");
+
+
+Console.WriteLine("Gib eine Zahlen ein:");
+int userZahl1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Gib  noch eine Zahlen ein:");
+int userZahl2= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sume von deine Zahlen ist: {userZahl1 + userZahl2}");
+
+
+Console.WriteLine("Gib eine Zahl ein:");
+int userZahl3= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(userZahl3 * userZahl3);
+*/
+
+//Aufgaben 2
+
+
+
+
+
+
+
+
+
+
+
+
